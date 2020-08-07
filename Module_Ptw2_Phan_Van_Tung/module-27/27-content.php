7703fb2dedf2976a1be27ac853f5a423eaeeb686
Phan Văn Tùng <33917113+Tungtungtang@users.noreply.github.com>
2020-08-08 00:43:22 +0700

    module_update

---
+++ Module_Ptw2_Phan_Van_Tung/module-27/27-content.php
@@ -0,0 +1,48 @@
+<div class="type-27">
+    <div class="container">
+        <div class="row form">
+            <div class="col-md-6">
+                <select class="select-top-left" name="phanloai" id="phanloai">
+                    <option value="date">Date Published</option>
+                    <option value="dog">Date Updated</option>
+                    <option value="cat">Title</option>
+                    <option value="hamster">Category</option>
+                </select>
+            </div>
+            <div class="col-md-6 select-right">
+                <select class="select-top-right" name="stt" id="stt">
+                    <option value="asc">Asc</option>
+                    <option value="desc">Desc</option>
+                </select>
+            </div>
+        </div>
+        <div class="form">
+            <input class="input-title" type="text" placeholder="Title">
+        </div>
+        <div class="form">
+            <select class="categories" name="Categories" id="Categories">
+                <option value="all-categories">All Categories</option>
+                <option value="bars">Bars & Pubs</option>
+                <option value="destinations">Destinations</option>
+                <option value="nightlife">Nightlife</option>
+                <option value="beautiful">Beautiful</option>
+                <option value="food">Food & Drink</option>
+                <option value="hotels">Hotels</option>
+                <option value="shopping">Shopping</option>
+                <option value="restaurants">Restaurants</option>
+            </select>
+        </div>
+        <div class="form">
+            <select class="locations" name="Locations" id="Locations">
+                <option value="all-locations">All Locations</option>
+                <option value="italia">Italia</option>
+                <option value="bangkok">Bangkok</option>
+                <option value="France">France</option>
+                <option value="united-state">United State</option>
+                <option value="arizona">-Arizona</option>
+                <option value="new-york">-New York</option>
+                <option value="florida">-Florida</option>
+            </select>
+        </div>
+    </div>
+</div>
